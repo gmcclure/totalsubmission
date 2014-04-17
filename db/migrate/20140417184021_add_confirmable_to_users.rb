b4929d269be7f1f46dfe7409261d46d64597e673
Greg McClure <gmcclure@gmail.com>
2014-04-17 11:45:56 -0700

    Added user signup confirmation.

---
+++ db/migrate/20140417184021_add_confirmable_to_users.rb
@@ -0,0 +1,15 @@
+class AddConfirmableToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :confirmation_token, :string
+    add_column :users, :confirmed_at, :datetime
+    add_column :users, :confirmation_sent_at, :datetime
+    add_column :users, :unconfirmed_email, :string
+
+    add_index :users, :confirmation_token,   :unique => true
+  end
+
+  def drop
+    remove_column :users, :unconfirmed_email,
+      :confirmation_token, :confirmed_at, :confirmation_sent_at
+  end
+end
