b81f187fad6bead43d2c322a8e43e8df842c72ca
Greg McClure <gmcclure@gmail.com>
2014-04-18 11:22:15 -0700

    Initial testing setup. Futzing with rspec and R4.1.

--- spec/models/publication_spec.rb
+++ spec/models/publication_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Publication do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(FactoryGirl.build(:publication)).to be_valid
+  end
+
+  it "is valid with a title, slug, and at least one user"
 end
