cd4149de9b17805f8604f5cfb0e5e4d1a93147ab
Greg McClure <gmcclure@gmail.com>
2014-04-18 12:17:59 -0700

    More FG setup. Users have a valid factory for the moment.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,9 @@
 
 FactoryGirl.define do
   factory :user do
+    email { Faker::Internet.email }
+    p = Faker::Internet.password(8)
+    password p
+    password_confirmation p
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have a valid factory" do
+    expect(FactoryGirl.build(:user)).to be_valid
+  end
 end
